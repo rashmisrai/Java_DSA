fb2bcfee0177d5b1a351577d905c20c6d07b415f
Rashmi.S.Rai <rashmirai6870@gmail.com>
2023-10-07 20:29:49 +0530

    Module Codes

---
+++ mod/CollegeAdmission.java
@@ -0,0 +1,42 @@
+package com.mod;
+
+import java.util.Scanner;
+public class CollegeAdmission {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Applicant name ");
+        String name=sc.nextLine();
+
+        System.out.println("Marks obtained in HSC ");
+        int mark_HSC=sc.nextInt();
+
+        System.out.println("Total possible marks in HSC ");
+        int Total_HSC=sc.nextInt();
+
+        System.out.println("Engineering cutoff mark ");
+        float mark_Engineering=sc.nextFloat();
+
+        System.out.println("Marks obtained in SSLC ");
+        int mark_SSLC=sc.nextInt();
+
+        System.out.println("Total possible marks in SSLC ");
+        int Total_SSLC=sc.nextInt();
+
+        System.out.println("Gender ");
+        char gender=sc.next().charAt(0);
+
+        System.out.println("Your Application has been Submitted Successfully");
+        System.out.println("The name of the applicant: " + name);
+        System.out.println("Engineering Cutoff: "+ mark_Engineering);
+        System.out.println("Applicant gender: "+ gender);
+        System.out.println("All the best for your Career");
+
+    }
+
+
+}
+
+
+
